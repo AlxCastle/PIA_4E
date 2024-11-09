1c8df715068e506937ed166b1706c623048674d7
AlxCastle <alondra.castillognz@uanl.edu.mx>
2024-11-09 09:50:41 -0600

    main de python

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# PIA_4E
-4o Entregable - Proyecto Final de Ciberseguridad: Integración de Módulos y Generación de Reportes 
+# 3o Entregable - Proyecto Final de Ciberseguridad: Módulos de Python y GitHub 🛡️💻
+
+## Descripción
+Este proyecto consiste en la creación de cuatro scripts en Python que realizan diversas tareas de ciberseguridad: análisis de conexiones TCP, detección de servicios sospechosos, consumo de APIs (Shodan e IPAbuseDB), y la implementación de un honeypot SSH. Los scripts aceptan parámetros de entrada y cuentan con menús interactivos para diversas funcionalidades, así como el manejo de errores durante la ejecución.
+
+## Objetivos
+- Desarrollar habilidades en Python: Aplicar lo aprendido sobre Python para crear scripts que realicen tareas de ciberseguridad.
+
+## Scripts
+
+1. **analyze_connections.py**
+   - Este script analiza las conexiones TCP activas en el sistema y genera un informe sobre las conexiones sospechosas basándose en puertos no estándar.
+
+2. **suspicious_services.py**
+   - Este script obtiene los servicios en ejecución en el sistema, identifica aquellos que son sospechosos (por ejemplo, los que están deshabilitados o no están en ejecución), y genera un informe en un archivo de texto. Además, ofrece la opción de exportar los resultados a un archivo de Excel.
+
+3. **modules_api.py**
+   - Este script interactúa con las APIs de Shodan e IPAbuseDB para buscar vulnerabilidades y detectar direcciones IP sospechosas.
+
+4. **honeypot_ssh.py**
+   - Este script permite crear un honeypot SSH en el sistema, simulando un servidor SSH de kali linux para atraer posibles atacantes.
+
+## Requisitos
+- Python 3.x
+- Paquetes de Python: `paramiko`, `Requests`, `termcolor`, `shodan`, `openpyxl`
+- Tener acceso a PowerShell en Windows para ejecutar ciertos comandos.
+
+Instala los paquetes necesarios utilizando:
+
+```bash
+pip install -r requirements.txt
+```  
+
+## Instrucciones de Uso
+Clona el repositorio en tu máquina local:
+
+```bash
+git clone https://github.com/AlxCastle/PIA_3E.git
+```
+
+Ahora cuentas con la carpeta y los scripts.
+
+### Ejecución del script principal:
+
+El script principal permite seleccionar diferentes opciones a través de un menú interactivo:
+
+```bash
+python main.py
+```
+
+Las opciones disponibles son:
+
+- **[1]** Iniciar un honeypot SSH
+- **[2]** Consultar la API de Shodan
+- **[3]** Consultar la API de IPAbuseDB
+- **[4]** Analizar conexiones TCP
+- **[5]** Detectar servicios sospechosos
+- **[0]** Salir
+
+### Ejemplo de Ejecución
+
+Ejemplo 1: Ejecutar el análisis de conexiones y guardar el informe en un archivo específico:
+
+```bash
+python main.py
+```
+
+Seleccione la opción 4 y siga las instrucciones que da el programa.
+
+## Demostración
+Puedes ver un video corto de la explicación y ejecución de los scripts aquí: https://youtu.be/bmVoowfu71o
+
+## Colaboradores
+Este proyecto fue desarrollado en colaboración con:
+
+- Emilio Rafael Puente Cardona
+- Manuel Emilio Delgado Gómez
+- Alondra Castillo Gonzalez
+  
---
+++ main.py
@@ -0,0 +1,85 @@
+from termcolor import colored
+from Modules.honeypot_ssh import start_honeypot
+from Modules.modules_api import search_vulnerabilities, suspicious_ip
+from Modules.analyze_connections import analyze_connections
+from Modules.suspicious_services import suspicious_services
+
+#APIKEY for shodan suggested: skoTKeGUubhAIZbKPZEBpEeEiuk8o5Wu
+#APIKEY for IPAbuseDB suggested: 51bffcedf179e67ae15996a1160b04cacb0e23f49841aa355b2602e8335e2cf692c698c93033e9a6
+
+def format_menu():
+    """This function contains the format of the menu."""
+    msg = "-"*50+"""
+        MENU
+    [0]. Salir
+    [1]. Iniciar SSH Honeypot
+    [2]. Consultar API Shodan
+    [3]. Consultar API IPAbuseD
+    [4]. Analizar conexiones
+    [5]. Servicios sospechosos
+"""
+    print(msg)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            format_menu()
+            option = int(input("Seleccione una opcion: "))
+            
+            if option == 1: 
+                port = input("Puerto para vincular el servidor SSH (default 2222): ")
+                
+                #Validate the port.
+                while True:
+                    try: 
+                        if port == "":
+                            port = 2222
+                            break 
+                        else: 
+                            port = int(port)
+                            while port < 1024 or port > 65535:
+                                port = int(input("Ingrese el puerto en un rango de 1024-65535: "))
+                            break 
+                    except ValueError: 
+                        port = input("Es un dato numérico, si no desea ingresar un puerto presione enter: ")
+                start_honeypot(port)
+                
+            elif option == 2:
+                APIKEY=input("Ingrese la API key que se usara para conectarse a la API de shodan")
+                ports=input("Ingrese los puertos que quiera ver, en caso de ser mas de uno separarlos por una coma y un espacio")
+                port_shodan="port: "+str(ports)
+                search_vulnerabilities(APIKEY,port_shodan)
+                
+            elif option == 3:
+                APIKEY=input("Ingrese la API key que se usara para conectarse a la API de IPAbuseDB")
+                suspicious_ip(APIKEY)
+                
+            elif option == 4:
+                try:
+                    opcion_nombre=int(input("Desea editar el nombre del archivo en donde se guardará el reporte (suspicious_connections_report.txt)? 1-Si 2-No: "))
+                    while (opcion_nombre <=0 or opcion_nombre>2):
+                        opcion_nombre = int(input("Opicón no válida, intente nuevamente: "))
+                    if opcion_nombre == 1:
+                        nombre_reporte=input("Ingrese el nombre con el que se guardará el reporte (incluya la extensión .txt): ")
+                        analyze_connections(nombre_reporte)
+                    else:
+                        analyze_connections()
+                except Exception: 
+                    print("Ha ocurrido un error, intente nuevamente")
+
+            elif option == 5:
+                try:
+                    opcion_generar_excel = int(input("Deseas conservar el excel con los procesos activos? 1-Sí 2-No: "))
+                    while opcion_generar_excel<1 or opcion_generar_excel>2:
+                        opcion_generar_excel = int(input("Opción no válida, vuleve a ingresarla: "))
+                    suspicious_services(opcion_generar_excel)
+                except Exception:
+                    print("Ha ocurrido un error, intentalo de nuevo")
+                
+            elif option == 0:
+                print("Saliendo...")
+                break
+            else:   
+                print(colored("Opcion incorrecta.", 'red'))
+        except ValueError:
+            print(colored("Ingrese un valor de tipo numerico.", 'red'))
