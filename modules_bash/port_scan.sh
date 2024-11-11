ef510febc98cd24168264fe7237a781fe5e342ee
AlxCastle <alondra.castillognz@uanl.edu.mx>
2024-11-11 15:39:12 -0600

    Update port_scan.sh

--- modules_bash/port_scan.sh
+++ modules_bash/port_scan.sh
@@ -4,6 +4,7 @@
 target="$1"          # Primer parámetro: IP o dominio
 port_range="$2"      # Segundo parámetro: Rango de puertos
 name="${3:-scan_results}"  # Tercer parámetro: Nombre del archivo (por defecto: scan_results)
+scan_udp=false                 # Activar escaneo UDP
 
 # Función para manejar errores
 handle_error() {
@@ -16,6 +17,12 @@ trap handle_error ERR
 
 # Función para escanear puertos TCP usando nmap
 scan_ports_tcp() {
+    if [ -z "$target" ] || [ -z "$port_range" ]; then
+        read -p "Enter the IP or domain to scan: " target
+        read -p "Enter the port range to scan (example: 1-1000): " port_range
+    fi
+
+    # Obtener la fecha y hora actuales
     timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
     echo "Scanning TCP ports on $target..."
@@ -60,6 +67,11 @@ scan_ports_tcp() {
 
 # Función para escanear puertos UDP usando nmap
 scan_ports_udp() {
+    if [ -z "$target" ] || [ -z "$port_range" ]; then
+        read -p "Enter the IP or domain to scan: " target
+        read -p "Enter the UDP port range to scan (example: 1-1000): " port_range
+    fi
+
     timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
     echo "Scanning UDP ports on $target..."
@@ -107,6 +119,30 @@ show_menu() {
     echo "===================="
 }
 
+#Process the parameters
+while getopts ":t:p:ru" opt; do
+    case $opt in
+        t) target="$OPTARG" ;; #IP or domain
+        p) port_range="$OPTARG" ;; #Port range
+        r) auto_generate_report=true ;; #Automatically generate the report
+        u) scan_udp=true ;; #Enable UDP scan
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+#Check if input parameters were provided
+if [ -n "$target" ] && [ -n "$port_range" ]; then
+    if [ "$scan_udp" = true ]; then
+        scan_ports_udp
+    else
+        scan_ports_tcp
+    fi
+    exit 0
+fi
+
 #Loop of the main menu
 while true; do
     show_menu
@@ -123,6 +159,9 @@ while true; do
             ;;
     esac
 done
+
+
+
 
 
 
