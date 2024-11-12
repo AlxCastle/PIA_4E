39866b2d2fad389f491fc745491b317a587aeec5
AlxCastle <alondra.castillognz@uanl.edu.mx>
2024-11-11 21:52:17 -0600

    Update honeypot_ssh.py

--- Modules/honeypot_ssh.py
+++ Modules/honeypot_ssh.py
@@ -6,17 +6,6 @@ import paramiko
 from termcolor import colored
 from datetime import datetime
 
-
-#Configure basic logging settings
-log = logging.getLogger('ssh_honeypot')
-log.setLevel(logging.INFO)
-
-file_handler = logging.FileHandler('report_ssh_honeypot.log')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-log.addHandler(file_handler)
-
-
 #Global variable to control stopping the SSH honeypot
 stop_server = False  
 
@@ -25,8 +14,8 @@ private_key = paramiko.RSAKey.generate(2048)
 private_key.write_private_key_file('server.key')
 HOST_KEY = paramiko.RSAKey(filename='server.key')
 
-#SSH banner emulating a Kali Linux server
-SSH_BANNER = "SSH-2.0-OpenSSH_8.7p1 Debian-3ubuntu0.1"  
+#SSH banner emulating a Ubuntu Linux server
+SSH_BANNER = "SSH-2.0-OpenSSH_8.3p1 Ubuntu-1ubuntu1"  
 
 
 def get_local_ip():
@@ -37,11 +26,19 @@ def get_local_ip():
         sock.connect(('8.8.8.8', 80))
         local_ip = sock.getsockname()[0]
     except Exception as e:
-        log.error(f"Error obtaining local IP: {e}")
+        logging.error(f"Error obtaining local IP: {e}")
         local_ip = '127.0.0.1' 
     finally:
         sock.close()
     return local_ip
+
+
+#Configure basic logging settings
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+    filename='report_ssh_honeypot.log'
+)
 
 
 class SshHoneypot(paramiko.ServerInterface):
@@ -56,19 +53,24 @@ class SshHoneypot(paramiko.ServerInterface):
         """Allows 'session' or 'shell' channel requests.
         Logs the request type and client IP."""
         if kind in ['session', 'shell']:
-            log.info(f'Channel request: {self.client_ip} ({kind})')
+            logging.info(f'Channel request: {self.client_ip} ({kind})')
             return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
+    
+    ##
+    def check_channel_pty_request(self, channel, term, width, height, width_pixels, height_pixels, encoding=None):   
+        logging.info(f'PTY requested by {self.client_ip} with terminal: {term}')
+        return True
 
     def get_allowed_auths(self, username):
         """Returns the allowed authentication method as 'password'.
         Logs the username and client IP for authentication attempts."""
-        log.info(f'Auth requested: {self.client_ip} (user: {username})')
+        logging.info(f'Auth requested: {self.client_ip} (user: {username})')
         return "password"
 
     def check_auth_password(self, username, password):
         """Logs the password attempt and simulates successful authentication."""
-        log.info(f'Password attempt: {self.client_ip} (user: {username}, password: {password})')
+        logging.info(f'Password attempt: {self.client_ip} (user: {username}, password: {password})')
         self.username = username
         return paramiko.AUTH_SUCCESSFUL
 
@@ -86,32 +88,35 @@ def handle_command(command, msg, user):
     command_parts = command.split() 
 
     if command == "ls":
-        response = colored("Desktop    Downloads   Documents    Music    Pictures\r\n", 'blue', attrs=['bold'])
+        response = colored("\nDesktop  Documents  Downloads  Music  Pictures  Public  snap  Templates  Videos\r\n", 'blue', attrs=['bold'])
     elif command == "pwd":
-        response = f"/home/{user}\r\n"
+        response = f"\n/home/{user}\r\n"
     elif command == "whoami":
-        response = f"{user}\r\n"
+        response = f"\n{user}\r\n"
     elif command == "date":
-        response = datetime.now().strftime("%a %b %d %I:%M:%S %p %Z %Y") + "\r\n"
+        response = datetime.now().strftime("\n%a %b %d %I:%M:%S %p %Z %Y") + "\r\n"
     elif "echo" in command:
         text = " ".join(command_parts[1:])
-        response =  (f"{text}\r\n") if len(command_parts) > 1 else "\r\n"
+        response =  (f"\n{text}\r\n") if len(command_parts) > 1 else "\r\n"
     elif "cat" in command:
         if len(command_parts) > 1:
-            response = "This is just a text.\r\n" 
+            response = "\nThis is just a text.\r\n" 
         else:
-            response= "cat: missing operand\r\n"
+            
+            response= f"\ncat: {command}: No such file or directory\r\n"
     else:
-        response = f"{command}: Command not found\r\n"
+        if command == "":
+                response = "\n"
+        else:
+            response = f"\n{command}: command not found\r\n"
     
     msg.send(response.encode("utf-8"))  # Send response as bytes
 
 
 def handle_connection(client, addr):
-    """Manages incoming connections, handles authentication and commands for the honeypot.
-    Logs connection attempts and simulates a shell prompt."""
+    """Manages incoming connections, handles authentication and commands for the honeypot."""
     client_ip = addr[0]
-    log.info(f'New connection from: {client_ip}')
+    logging.info(f'New connection from: {client_ip}')
 
     transport = paramiko.Transport(client)
     transport.add_server_key(HOST_KEY)
@@ -122,41 +127,44 @@ def handle_connection(client, addr):
         transport.start_server(server=server)
         msg = transport.accept(20)
         if msg is None:
-            log.warning(f"No message received from {client_ip} within the timeout.")
+            logging.warning(f"No message received from {client_ip} within the timeout.")
             return
 
-        time.sleep(1)
+        time.sleep(0.5)
         date = datetime.now().strftime("%a %b %d %I:%M:%S %p %Z %Y") + "\r\n"
-        msg.send(f"Welcome to the Kali Linux SSH server\r\nLast login: {date}".encode("utf-8"))
+        msg.send(f"Welcome to the Ubuntu Linux SSH server\r\nLast login: {date}".encode("utf-8"))
 
-        time.sleep(5)
         while True:
             user = server.username  
-            #Simulate a Linux shell prompt
-            prompt_part1 = colored("┌──(", 'light_green')
-            prompt_part2 = colored(f"{user}㉿kali", 'blue', attrs=['bold'])  
-            prompt_part3 = colored(")-[~]", 'light_green')
-            prompt_part4 = colored("└─", 'light_green')
-            prompt_part5 = colored("$ ", 'blue', attrs=['bold'])
+            # Simulate a Ubuntu Linux shell prompt
+            prompt_part1 = colored(f"{user}@localhost",color="green", attrs=['bold'])
+            prompt_part2 = colored(":", color="white", attrs=['bold'])
+            prompt_part3 = colored("~", color="blue", attrs=['bold'])
+            prompt_part4 = colored("$", color="white")
             format_prompt = (
-                f"{prompt_part1}{prompt_part2}{prompt_part3}\n{prompt_part4}{prompt_part5}"
+                f"{prompt_part1}{prompt_part2}{prompt_part3}{prompt_part4} "
             )
             msg.send(format_prompt.encode("utf-8")) 
 
-            try:
-                command = msg.recv(1024).decode("utf-8").strip()
-                if command == "exit":
-                    log.info(f'Command from {client_ip}: {command}')
-                    break
-                else:
-                    log.info(f'Command from {client_ip}: {command}')
-                    handle_command(command, msg, user)
-            except Exception as e:
-                log.error(f'Error processing command from {client_ip}: {e}')
-                msg.send(f"Error processing command: {e}\r\n".encode("utf-8"))
+            command_buffer = "" 
+            while True:
+                command_char = msg.recv(1).decode("utf-8")  
+                msg.send(command_char.encode("utf-8")) 
+                if command_char == "\r" or command_char == "\n":  
+                    break 
+                command_buffer += command_char  
 
+            command = command_buffer.strip()  
+            logging.info(f'Command from {client_ip}: {command}')
+            if command == "exit":
+                logging.info(f'Command from {client_ip}: {command}')
+                break
+            else:
+                logging.info(f'Command from {client_ip}: {command}')
+                handle_command(command, msg, user)
+               
     except Exception as e:
-        log.error(f'Error handling connection from {client_ip}: {e}')
+        logging.error(f'Error handling connection from {client_ip}: {e}')
         try: 
             if msg:
                 msg.send("An error occurred in the connection.\r\n".encode("utf-8"))
@@ -168,11 +176,10 @@ def handle_connection(client, addr):
         msg.close()
         transport.close()
 
-
 def capture_user_input():
     """Waits for user input to stop the honeypot server"""
     global stop_server
-    x = input("Presione Enter para detener la honeypot...")
+    x = input("Press Enter to stop the honeypot...")
     stop_server = True
 
 
@@ -184,10 +191,10 @@ def start_server(port, bind_addr):
     sock.listen(100)
     sock.settimeout(1)  
 
-    log.info(f'Server listening on {bind_addr}:{port}')
+    logging.info(f'Server listening on {bind_addr}:{port}')
     print(f"""
           **SSH HONEYPOT**
-Servidor escuchando en {bind_addr} : {port} \n""")
+Server listening on {bind_addr} : {port} \n""")
 
     while not stop_server:
         try:
@@ -196,9 +203,9 @@ Servidor escuchando en {bind_addr} : {port} \n""")
         except socket.timeout:
             continue
         except Exception as e:
-            log.error(f'Error accepting connection: {e}')
+            logging.error(f'Error accepting connection: {e}')
 
-    print("Honeypot Detenida.")
+    print("Honeypot stopped.")
 
 
 def start_honeypot(port):
